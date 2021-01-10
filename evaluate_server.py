2da253b6665f973a7419f5231137d73dd1e0ded5
Black <ioumu8@gmail.com>
2021-01-10 16:24:46 +0800

    Add evaluate_server
    Build a evaluate_server to handle evaluting processes.

---
+++ evaluate_server.py
@@ -0,0 +1,24 @@
+"""
+Server to handle processes of evaluating.
+"""
+
+import pickle
+from flask import Flask
+from flask import request
+from thousandaire.evaluator import Evaluator
+
+APP = Flask(__name__)
+
+@APP.route('/', methods=['POST'])
+def evaluate():
+    """
+    Connect with evaluator.
+    """
+    eval_data = pickle.loads(request.get_data())
+    indicator_names = eval_data['indicator_names']
+    instruments = eval_data['instruments']
+    data = eval_data['data']
+    return pickle.dumps(Evaluator(indicator_names).run(instruments, data))
+
+if __name__ == '__main__':
+    APP.run(debug=False)
